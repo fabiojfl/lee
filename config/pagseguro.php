8ccf172cd3f6fae1d99bf28294c65a550bcc1b25
Fabiojs <fabiojfl@gmail.com>
2017-03-16 20:45:37 -0300

    pag

--- config/pagseguro.php
+++ config/pagseguro.php
@@ -3,7 +3,8 @@
 return [
     'environment' => 'production', //sandbox 
     'email' => 'leeleecosmeticos@gmail.com',
-    'token' => '0913D6179E574201A6B65EDA5B23E9BD'
+	'token' => '59C989D6368F4D03990EAE3E52FEB82C'
 ];
 
+//'token' => '0913D6179E574201A6B65EDA5B23E9BD'//sandbox
 
